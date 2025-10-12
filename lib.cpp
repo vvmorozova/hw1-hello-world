0f44752b5260dd855099dc78536cae40771b9056
vvmorozova <vvmorozova_1@miem.hse.ru>
2025-10-12 17:16:38 +0300

    optimisation

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,17 @@
 #include "lib.h"
 
 #include <algorithm>
-#include <cassert>
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <functional>
+#include <array>
 
 #define BYTES_IN_IPV4 4
 #define IP_PLACE_IN_INPUT_STR 0
+
+using IP_t = std::array<u_int8_t, 4>;
+
 // string std is split into vector of strings by delimiter d
 std::vector<std::string> split(const std::string &str, char d) {
   std::vector<std::string> r;
@@ -28,72 +30,62 @@ std::vector<std::string> split(const std::string &str, char d) {
   return r;
 }
 
+//converts std::string to IP_t = std::array<u_int8_t, 4>
+IP_t strToIP_t(const std::vector<std::string> &splitted) {
+  IP_t ip;
+  for (auto i = 0; i < BYTES_IN_IPV4; i++) {
+    ip[i] = static_cast<u_int8_t>(std::stoi(splitted[i]));
+  }
+  return ip;
+}
+
 // if an IP address from the IP pool satisfies the canBePrint condition, it is printed to stdout
 // if canBePrint is not specified, all ips are printed
-void print_ips(const std::vector<std::vector<std::string>> &ip_pool,
-                std::function<bool(const std::vector<std::string> &)> canBePrint = nullptr)
+void print_ips(const std::vector<IP_t> &ip_pool,
+                std::function<bool(const IP_t &)> canBePrint = nullptr)
 {
-  for (auto currIP : ip_pool) {
+  for (const auto &currIP : ip_pool) {
 
-    if (canBePrint) {
-      if (!canBePrint(currIP)) {
-        continue;
+    if (canBePrint && !canBePrint(currIP)) {
+      continue;
+    }
+    for (auto i = 0; i < BYTES_IN_IPV4; i++) {
+      std::cout << static_cast<int>(currIP[i]);
+      if (i < BYTES_IN_IPV4 - 1) {
+        std::cout << ".";
       }
     }
-      int i = 0;
-      for (auto currTet : currIP) {
-        std::cout << currTet;
-        if (i++ < BYTES_IN_IPV4 - 1) {
-          std::cout << ".";
-        }
-      }
-      std::cout << std::endl;
-    }
+    std::cout << std::endl;
+  }
 }
 
 int ip_filter() {
   try {
 
-    std::vector<std::vector<std::string>> ip_pool;
+    std::vector<IP_t> ip_pool;
 
     for (std::string line; std::getline(std::cin, line);) {
       std::vector<std::string> str = split(line, '\t');
-      ip_pool.push_back(split(str.at(IP_PLACE_IN_INPUT_STR), '.'));
+      auto currIP = strToIP_t(split(str.at(IP_PLACE_IN_INPUT_STR), '.'));
+      ip_pool.push_back(currIP);
     }
 
-    // reverse lex comparison algo
-    auto revLex = [](std::vector<std::string> fisrtIP,
-                       std::vector<std::string> secondIP) -> bool {
-      for (int i = 0; i < BYTES_IN_IPV4; i++) {
-        int diff = std::stoi(fisrtIP[i]) - std::stoi(secondIP[i]);
-        if (diff != 0) {
-          return diff > 0 ? true : false;
-        }
-      }
-      return true;
-    };
-    std::sort(ip_pool.begin(), ip_pool.end(), revLex);
+    std::sort(ip_pool.begin(), ip_pool.end(), std::greater<IP_t>());
     print_ips(ip_pool);
     
-    auto firstByteIs1 = [](std::vector<std::string> ip) -> bool {
-      if (ip[0] == "1") {
-        return true;
-      }
-      return false;
+    auto firstByteIs1 = [](const IP_t &ip) -> bool {
+      return ip[0] == 1;
     };
     print_ips(ip_pool, firstByteIs1);
     
-    auto f46s70 = [](std::vector<std::string> ip) -> bool {
-      if (ip[0] == "46" && ip[1] == "70") {
-        return true;
-      }
-      return false;
+    auto f46s70 = [](const IP_t &ip) -> bool {
+      return ip[0] == 46 && ip[1] == 70;
     };
     print_ips(ip_pool, f46s70);
 
-    auto any46 = [](std::vector<std::string> ip) -> bool {
+    auto any46 = [](const IP_t &ip) -> bool {
       for (int i = 0; i < BYTES_IN_IPV4; i++) {
-        if (ip[i] == "46") {
+        if (ip[i] == 46) {
           return true;
         }
       }
