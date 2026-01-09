82fc6ba29c3cd7ec9738d86ec99e63fbf016df82
vvmorozova <vvmorozova_1@miem.hse.ru>
2026-01-09 14:42:28 +0300

    tuple works

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int, char **) {
 	print_ip( std::string{"Hello, World!"} ); // Hello, World! 
 	print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
 	print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100 
-	//print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+	print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
 	return 0;
 } 
 
--- template_magic.h
+++ template_magic.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <cstdint>
+#include <vector>
+#include <list>
 
 template <typename T,
              std::enable_if_t<std::is_integral<T>::value, bool> = true>
@@ -34,9 +36,21 @@ public:
     static constexpr bool value = decltype(test<T>(0))::value;
 };
 
-template <typename Container,
-          typename = std::enable_if_t<is_printable<typename Container::value_type>::value>>
-void print_ip(const Container& cont) {
+// check if container vector of list
+template <typename T>
+struct is_vec_or_list : std::false_type {};
+
+template <typename T, typename Alloc>
+struct is_vec_or_list<std::vector<T, Alloc>> : std::true_type {};
+
+template <typename T, typename Alloc>
+struct is_vec_or_list<std::list<T, Alloc>> : std::true_type {};
+
+// vector or list overload
+template <typename Container, std::enable_if_t<is_printable<typename Container::value_type>::value && 
+		  							is_vec_or_list<Container>::value, bool> = true>
+auto print_ip(const Container& cont)
+{
 	int i = 0;
 	int len = cont.size();
     for (const auto& elem : cont) {
@@ -47,4 +61,48 @@ void print_ip(const Container& cont) {
 		i++;
     }
     std::cout << std::endl;
+}
+
+// for iterating the tuple
+template<std::size_t I = 0, typename... Ts>
+typename std::enable_if<I == sizeof...(Ts), void>::type
+print_tuple(const std::tuple<Ts...>&)
+{}
+
+template<std::size_t I = 0, typename... Ts>
+typename std::enable_if<I < sizeof...(Ts), void>::type
+print_tuple(const std::tuple<Ts...>& t)
+{
+    std::cout << std::get<I>(t);
+    if (I + 1 < sizeof...(Ts))
+        std::cout << ".";
+    print_tuple<I + 1>(t);
+}
+
+// template <typename TupleT, std::size_t TupSize = std::tuple_size_v<TupleT>>
+// void printTuple(const TupleT& tp) {
+//     printTupleImp(tp, std::make_index_sequence<TupSize>{});
+// }
+
+// check if all types in type are same
+template <typename... T>
+struct all_same_types : std::true_type {};
+
+template <typename F, typename S, typename... Other>
+struct all_same_types<F, S, Other...> : std::conditional_t<std::is_same<F, S>::value,
+										all_same_types<S, Other...>, std::false_type> {};
+
+template <typename T_TUPLE>
+struct is_all_same_tuple;
+
+template <typename... Types>
+struct is_all_same_tuple<std::tuple<Types...>> : all_same_types<Types...> {};
+
+
+// tuple overload
+template <typename TUPLE_T>
+auto print_ip(TUPLE_T const& t) -> std::enable_if_t<is_all_same_tuple<TUPLE_T>::value, void>
+{
+	print_tuple(t);
+	std::cout << std::endl;
 }
