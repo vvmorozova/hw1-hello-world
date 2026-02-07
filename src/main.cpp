d3421ee858b3c5d4d3333c7c3a6afe0f58c4cb0f
vvmorozova <vvmorozova_1@miem.hse.ru>
2026-02-07 22:16:34 +0300

    move logic to controller. todo: make it compilable and move code to different files

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <string>
 
 enum class currElemTypeToDraw {
 	Line,
@@ -14,9 +15,27 @@ enum class currElemTypeToDraw {
 
 namespace View {
 	class UI {
+	private:
+		std::shared_ptr<class Controller::Logic> m_controller;
 
-	};
+	public:
+		void onMouseDown(int x, int y);
+		void onMouseUp(int x, int y);
+		void onMouseMove(int x, int y);
 
+		void onNewDoc();
+		void onImportDoc(const std::string& filename);
+		void onExportDoc(const std::string& filename);
+
+		void setController(std::shared_ptr<class Controller::Logic> controller)
+		{
+			m_controller = controller;
+		}
+	};	
+
+}
+
+namespace Model {
 	struct coordinates {
 		int x;
 		int y;
@@ -28,28 +47,24 @@ namespace View {
 		uint8_t b;
 	};
 
+
 	class Elem {
 	private:
-
 		coordinates basePoint;
 		RGB color;
 		int layer;
 
 	public:
+		virtual ~Elem();
 
-		virtual int draw();
+		void setBasePoint(const coordinates &newBasePoint);
+		coordinates getBasePoint() const;
 
-		void setBasePoint(coordinates &newBasePoint);
-		coordinates getBasePoint();
+		void setColor(const RGB &newColor);
+		RGB getColor() const;
 
-		void setColor(RGB newColor);
-		RGB getColor();
-
-		void setLayer(int newLayer);
-		int getLayer();
-
-		virtual bool isPointInside(coordinates &point);
-		virtual void fill();
+		void setLayer(const int newLayer);
+		int getLayer() const;
 	};
 
 	class Line: public Elem {
@@ -57,10 +72,8 @@ namespace View {
 		coordinates secondPoint;
 
 	public:
-		int draw() override;
-
-		void setSecondPoint(coordinates &newSecondPoint);
-		coordinates getSecondPoint();
+		void setSecondPoint(const coordinates &newSecondPoint);
+		coordinates getSecondPoint() const;
 	};
 
 	class Circle: public Elem {
@@ -68,103 +81,134 @@ namespace View {
 		int radius;
 
 	public:
-		int draw() override;
-
-		void setRadius(coordinates &newRadius);
-		coordinates getRadius();
-
-		bool isPointInside(coordinates &point) override;
-		void fill() override;
+		void setRadius(const int &newRadius);
+		int getRadius() const;
 	};
 
 	class Rectangle: public Elem {
 	private:
-		coordinates SecondPoint;
+		coordinates secondPoint;
 
 	public:
-		int draw() override;
-
-		void setSecondPoint(coordinates &newSecondPoint);
-		coordinates getSecondPoint();
-
-		bool isPointInside(coordinates &point) override;
-		void fill() override;
+		void setSecondPoint(const coordinates &newSecondPoint);
+		coordinates getSecondPoint() const;
 	};
 
-}
-
-namespace Model {
 	class Storage {
 	private:
 		currElemTypeToDraw currType;
 		// vector of already drawed elems
-		std::vector<std::shared_ptr<View::Elem>> drawed;
+		std::vector<std::shared_ptr<Elem>> painted;
 		// cached last opened/exported file's names
 		std::queue<std::string> lastFiles;
-		// for ctrl Z ctrl Y
-		std::stack<int> cmdStack;
 	public:
-		void setCurrElemTypeToDraw(currElemTypeToDraw type);
-		currElemTypeToDraw getCurrElemTypeToDraw();
-
-		void addElem(std::shared_ptr<View::Elem>);
-		void removeElem(std::shared_ptr<View::Elem>);
-
+		void setCurrElemTypeToDraw(const currElemTypeToDraw &type);
+		currElemTypeToDraw getCurrElemTypeToDraw() const;
+	
+		void addElem(std::shared_ptr<Elem>);
+		void removeElem(std::shared_ptr<Elem>);
+	
 	};
 }
 
 namespace Controller {
-	class Logic {
-	private:
-		std::shared_ptr<Model::Storage> m_pS;
-		std::shared_ptr<View::UI> m_pUI;
+	class DrawCommand {
 	public:
-		Logic(std::shared_ptr<Model::Storage> pS, std::shared_ptr<View::UI> pUI) : m_pS(pS), m_pUI(pUI) {};
-
-		int run();
+		virtual ~DrawCommand();
+		virtual bool execute(std::shared_ptr<Model::Storage> storage);
 	};
 
-	class DrawCommand {
-	private:
-		int cmdId;
+	class DrawCircle : public DrawCommand{
 	public:
-		int execute(View::Elem element);
+		virtual bool execute(std::shared_ptr<Model::Storage> storage);
+	};
+
+	class DrawRectangle : public DrawCommand{
+	public:
+		virtual bool execute(std::shared_ptr<Model::Storage> storage);
+	};
+
+	class EraseCommand : public DrawCommand{
+	public:
+		virtual bool execute(std::shared_ptr<Model::Storage> storage);
+	};
+
+	class DrawLine : public DrawCommand{
+	public:
+		virtual bool execute(std::shared_ptr<Model::Storage> storage);
+	};
+
+	class RecolorCommand : public DrawCommand{
+	public:
+		virtual bool execute(std::shared_ptr<Model::Storage> storage);
 	};
 
 	class DocCommand {
-	private:
-		std::string m_filename;
 	public:
-		DocCommand (std::string filename) : m_filename(filename) {};
-		virtual int execute();
-
-		void setFilename();
-		std::string getFilename();
+		virtual ~DocCommand() {};
+		virtual bool execute(const std::string & filename);
 	};
 
 	class ImportDocCommand : public DocCommand {
 	public:
-		ImportDocCommand (std::string filename) : DocCommand(filename) {};
-		int execute() override;
+		bool execute(const std::string & filename) override;
 	};
 
 	class ExportDocCommand : public DocCommand {
 	public:
-		ExportDocCommand (std::string filename) : DocCommand(filename) {};
-		int execute() override;
+		bool execute(const std::string & filename) override;
 	};
 
 	class NewDocCommand : public DocCommand {
 	public:
-		NewDocCommand (std::string filename) : DocCommand(filename) {};
-		int execute() override;
+		bool execute(const std::string & filename) override;
+	};
+
+	void handleMouseDown(int x, int y);
+	void handleMouseUp(int x, int y);
+	void handleMouseMove(int x, int y);
+
+	void handleNewDoc();
+	void handleImportDoc(const std::string& filename);
+	void handleExportDoc(const std::string& filename);
+
+
+	class Renderer {
+	public:
+		void fill(const Model::coordinates &fillPoint, const Model::RGB &color);
+		void draw(const std::shared_ptr<Model::Elem> elem);
+	};
+
+	class Logic {
+	private:
+		std::shared_ptr<Model::Storage> m_pS;
+		std::shared_ptr<View::UI> m_pUI;
+
+		// for ctrl Z ctrl Y
+		std::stack<std::shared_ptr<Controller::DrawCommand>> cmdStack;
+
+	public:
+		Logic(std::shared_ptr<Model::Storage> pS, std::shared_ptr<View::UI> pUI) : m_pS(pS), m_pUI(pUI) {};
+
+		bool run() {
+			if (!m_pUI) {
+				return false;
+			}
+
+			m_pUI->setController(std::make_shared<Controller::Logic>(*this));
+
+			return true;
+		};
 	};
 
 }
 
 int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	auto s = std::make_shared<Model::Storage>;
+	auto ui = std::make_shared<View::UI>;
+
+	auto l = std::make_shared<Controller::Logic>(s, ui);
+	l->run();
 	return 0;
 } 
 
